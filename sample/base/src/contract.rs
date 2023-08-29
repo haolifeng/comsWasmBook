dcfa70aa9b4fcb842c288a7421d24cd03029c505
haolifeng <haolifengwang@126.com>
2023-08-29 12:27:37 +0800

    Update

--- sample/base/src/contract.rs
+++ sample/base/src/contract.rs
@@ -1,4 +1,4 @@
-use crate::msg::{GreetResp, InstantiateMsg, QueryMsg};
+use crate::msg::{ ExecuteMsg, GreetResp, InstantiateMsg, QueryMsg};
 use cosmwasm_std::{
     to_binary, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
 };
@@ -31,5 +31,49 @@ mod query {
         };
 
         Ok(resp)
+    }
+}
+
+#[allow(dead_code)]
+pub  fn execute (
+    deps:DepsMut,
+    _env: Env,
+    info :MessageInfo,
+    msg: ExecuteMsg,
+)->StdResult<Response> {
+    use ExecuteMsg::*;
+    match msg {
+        AddMembers { admins} => exec::add_members(deps, info, admins);
+        ExecuteMsg::Leave {}=> exec::leave(deps, info),
+    }
+}
+mod exec {
+    use cosmwasm_std::StdError;
+    use super::*;
+    pub fn add_members(
+        deps: DepsMut,
+        info: MessageInfo,
+        admins: Vec<String>
+    )-> StdResult<Response> {
+        let mut curr_admins = ADMINS.load(deps.storage)?;
+        if !curr_admins.contains(&info.sender) {
+            return Err(StdError::generic_err("Unauthorised access"));
+        }
+
+        let admins: StdResult<Vec<_>> = admins.into_iter().map(|addr| deps.api.addr_validate(&addr)).collect();
+
+        curr_admins.append(&mut admins?);
+        ADMINS.save(deps.storage, &curr_admins);
+
+        Ok(Response::new())
+
+    }
+
+    pub fn leave(deps: DepsMut, info: MessageInfo)-> StdResult<Response> {
+        ADMINS.update(deps.storage, move |admins|-> StdResult<_> {
+            let admins = admins.into_iter().filter(|admin| *admin != info.sender).collect();
+            Ok(admins)
+        })?;
+        Ok(Response::new())
     }
 }
--- sample/base/src/msg.rs
+++ sample/base/src/msg.rs
@@ -5,6 +5,12 @@ pub struct InstantiateMsg {
     pub admins: Vec<String>,
 }
 
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
+pub enum ExecuteMsg {
+    AddMembers { admins: Vec<String>},
+    Leave {},
+}
+
 #[derive(Serialize, Deserialize)]
 pub struct GreetResp {
     pub message: String,
