88fd8643021b57a1369e9b51f1dbdb6599592634
haolifeng <haolifengwang@126.com>
2023-09-05 17:00:59 +0800

    update jsDevs

---
+++ sample/execution_message/jsDevs/config.js
@@ -0,0 +1,19 @@
+let config = {
+
+    wallet:{
+        address:"osmo1v6l9eda6mp49zzeaezd2lamdge04qtwhwfdae4",
+        mnemonic:"drive organ stem speak melody spare ancient craft fun taste search identify girl object lesson write recall permit draw defy know brother pear coconut",
+    },
+    wallet2:{
+        mnemonic :"swamp evolve wire grunt resource twice glimpse elevator solution fresh that arrive amazing wish inhale kick rescue law visa glow obscure ignore casual news",
+        address  : "osmo1vzhn4rjqv9crxcccxmr88h75a5thjtd22h26s2"
+    },
+    RPC_ENDPOINT:"https://rpc.osmotest5.osmosis.zone",
+    scByteCodePath : "../exeSc/artifacts/exeSc.wasm",
+    code_id:3809,
+    scAddr: 'osmo1lmd5qydwjhvp325k6uf5r5pmj45gnthhxn53dklqmfwrs06zgenq50lmav',
+
+
+}
+
+module.exports = config;
---
+++ sample/execution_message/jsDevs/exec_add_members_err.js
@@ -0,0 +1,23 @@
+const getSigner = require('./wallet');
+const getSigner2 = require('./wallet2');
+const config = require('./config');
+
+const scTools = require('./scTools');
+const {getOfflineSignerProto} = require("cosmjs-utils");
+
+
+
+const f = async ()=>{
+    let signer = await getSigner2();
+    let user2Addr = config.wallet2.address;
+    let msgData = {
+        AddMembers:{
+            admins:[
+                user2Addr
+            ]
+        }
+    }
+
+    await scTools.exeucte(signer,config.scAddr, Buffer.from(JSON.stringify(msgData)) );
+}
+f();
